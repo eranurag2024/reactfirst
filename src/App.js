3447aa8a8e96f00e613cbd6530e1120155ef6cc3
eranurag <anurag.practice2024@gmail.com>
2024-09-10 17:31:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>Hello MKMP BSC IT/CS Students<br></br><br></br>
+        TYBSC IT<br></br> 
+        SYBSC IT<br></br> 
+        FYBSC CS / IT<br></br></h3>
+
+
+        <img src={logo} className="App-logo" alt="logo" />
+        <a className="App-link" href="https://github.com/eranurag2024" target="_blank" rel="noopener noreferrer">
+          Get Repository Visit GitHub
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
